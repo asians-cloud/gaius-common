79bc255683e36c30fc34aee103da2e2f9ee9ebdd
Komang Suryadana <suryadana80@gmail.com>
2022-03-28 12:18:09 +0800

    Redis retry task

--- gaius_common/utils/celery.py
+++ gaius_common/utils/celery.py
@@ -2,7 +2,7 @@ from celery import Task
 from redis.exceptions import ResponseError
 
 
-class RedisTaskWithRetry(celery.Task):
+class RedisTaskWithRetry(Task):
     autoretry_for = (ResponseError,)
     retry_kwargs = {'max_retries': 3}
     retry_backoff = True
