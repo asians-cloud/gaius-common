2acb5207cb1d111d740daece428be291f0c2e429
Komang Suryadana <suryadana80@gmail.com>
2022-07-08 18:16:28 +0800

    Healthcheck middleware

--- config/settings/base.py
+++ config/settings/base.py
@@ -154,6 +154,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#middleware
 MIDDLEWARE = [
+    "gaius_common.middleware.healthz.HealthCheckMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
