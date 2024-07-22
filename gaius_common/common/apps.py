58a77e6a448ae47982359e0df51c4a7413ab702c
vaibhavac <vaibhav.chotaliya@asians.cloud>
2024-07-22 13:38:26 +0530

    fix issue

--- gaius_common/common/apps.py
+++ gaius_common/common/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class CommonConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gaius_common.common'
-
-    def ready(self):
-        import gaius_common.signals
--- gaius_common/kong/apps.py
+++ gaius_common/kong/apps.py
@@ -4,5 +4,3 @@ from django.apps import AppConfig
 class KongConfig(AppConfig):
     name = 'gaius_common.kong'
 
-    def ready(self):
-        import gaius_common.signals
