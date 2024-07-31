825788608088e268b254e57497a81776ae3e74ba
vaibhavac <vaibhav.chotaliya@asians.cloud>
2024-07-31 08:51:11 +0530

    fix issue

--- gaius_common/middleware/changeLog.py
+++ gaius_common/middleware/changeLog.py
@@ -16,10 +16,12 @@ def add_to_request_context(key, value):
     _request_ctx.set(context)
 
 def get_ip_address(request):
+    x_real_ip = request.META.get('HTTP_X_REAL_IP')
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        # HTTP_X_FORWARDED_FOR can return multiple IP addresses in case of a proxy
-        # In this case, the first IP address is the client's real IP address
+    
+    if x_real_ip:
+        ip = x_real_ip
+    elif x_forwarded_for:
         ip = x_forwarded_for.split(',')[0]
     else:
         ip = request.META.get('REMOTE_ADDR')
